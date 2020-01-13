8304a2158adcc4c2735d95cb14c8318b37d27842
Sabrina <delrock876@gmail.com>
2020-01-13 14:07:39 -0800

    set up server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const {join} = require('path')
-const express =require('express')
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+//routes
+require('./app/routing/apiRoutes')(app)
+require('./app/routing/htmlRoutes')(app)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT)
