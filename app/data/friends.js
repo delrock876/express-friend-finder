5ae3e5a995ccbc380c8ed84242af2d9df5970974
Sabrina <delrock876@gmail.com>
2020-01-13 14:04:47 -0800

    added friends

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+Learn more or give us feedback
+var friends = [
+  {
+    name: "Sarahi",
+    photo: "#",
+    scores: [
+      3,
+      1,
+      5,
+      2,
+      1,
+      1,
+      2,
+      4,
+      2,
+      1
+    ]
+  },
+  {
+    name: "Aaron",
+    photo: "#",
+    scores: [
+      3,
+      2,
+      1,
+      1,
+      2,
+      1,
+      2,
+      4,
+      1,
+      4
+    ]
+  },
+  {
+    name: "Karen",
+    photo: "#",
+    scores: [
+      2,
+      2,
+      4,
+      4,
+      3,
+      3,
+      5,
+      5,
+      1,
+      1
+    ]
+  },
+  {
+    name: "Paul",
+    photo: "#",
+    scores: [
+      2,
+      1,
+      3,
+      5,
+      5,
+      3,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    name: "Bobbie",
+    photo: "#",
+    scores: [
+      2,
+      1,
+      5,
+      3,
+      2,
+      2,
+      2,
+      2,
+      3,
+      1
+    ]
+  },
+  {
+    name: "Pablo",
+    photo: "#",
+    scores: [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  }
+ 
+];
+
+module.exports = friends
